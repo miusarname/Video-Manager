e47325df701a4964911af9ecf240fbf468ed64b5
miusarname <oscarm.alvarezg@gmail.com>
2024-02-06 20:49:53 -0500

    test: :white_check_mark: Added tests for login logic, registration and user verification.
    Test passed Respect +

--- test/user.test.js
+++ test/user.test.js
@@ -1,6 +1,6 @@
 // Importa Mocha y las funciones que deseas probar
 import { describe, it } from 'mocha';
-import { createUser, allUsers, getUserById, updateUser, deleteUser, verifyUser } from '../app/user/infrastructure/repository/user.js';
+import { createUser, allUsers, getUserById, updateUser, deleteUser, verifyUser,checkDuplicateUser,getUserByLastNameAndEmail,sendVerificationEmail } from '../app/user/infrastructure/repository/user.js';
 import { expect } from 'chai';
 
 // Aquí es donde comienzan tus pruebas
@@ -79,6 +79,48 @@ describe('Funciones de la base de datos', () => {
 
       // Verifica si la eliminación fue exitosa
       expect(result.deletedCount).to.equal(1);
+    });
+  });
+
+  describe('checkDuplicateUser', () => {
+    it('Debería devolver false para un usuario no duplicado', async () => {
+      // Simula nombres y correos electrónicos que no están duplicados
+      const firstnames = 'John';
+      const emails = 'john@example.com';
+
+      // Llama a la función checkDuplicateUser con los nombres y correos electrónicos simulados
+      const isDuplicate = await checkDuplicateUser(firstnames, emails);
+
+      // Verifica si la función devuelve false
+      expect(isDuplicate).to.be.false;
+    });
+  });
+
+  describe('getUserByLastNameAndEmail', () => {
+    it('Debería devolver un objeto de usuario si existe', async () => {
+      // Simula apellidos y correos electrónicos que existen en la base de datos
+      const lastnames = 'apellido de prueba';
+      const emails = 'test@example.com';
+
+      // Llama a la función getUserByLastNameAndEmail con los apellidos y correos electrónicos simulados
+      const user = await getUserByLastNameAndEmail(lastnames, emails);
+
+      
+      // Verifica si la función devuelve un objeto de usuario
+      expect(user).to.be.an('object');
+      // Aquí podrías agregar más aserciones para verificar la estructura del usuario, etc.
+    });
+
+    it('Debería devolver null si el usuario no existe', async () => {
+      // Simula apellidos y correos electrónicos que no existen en la base de datos
+      const lastnames = 'Smith';
+      const emails = 'nonexistent@example.com';
+
+      // Llama a la función getUserByLastNameAndEmail con los apellidos y correos electrónicos simulados
+      const user = await getUserByLastNameAndEmail(lastnames, emails);
+
+      // Verifica si la función devuelve null
+      expect(user).to.be.null;
     });
   });
 });
